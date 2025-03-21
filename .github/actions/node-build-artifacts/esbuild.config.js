9ffd6cad6fe3b6ec1762a8f271de73196a56fae6
Eray Hanoğlu <e.hanoglu@panates.com>
2025-03-21 19:12:49 +0300

    dev: Development changes

--- .github/actions/node-build-artifacts/esbuild.config.js
+++ .github/actions/node-build-artifacts/esbuild.config.js
@@ -6,9 +6,5 @@ require("esbuild")
     target: "node20",
     format: "cjs",
     outfile: __dirname + "/dist/index.js",
-    // Fix for https://github.com/evanw/esbuild/pull/2067
-    banner: {
-      js: "import { createRequire } from 'module'; const require = createRequire(import.meta.url);",
-    },
   })
   .catch(() => process.exit(1));
