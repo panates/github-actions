f50b5bc35eca8072f7b68dae7369227297cb54d8
Eray Hanoğlu <e.hanoglu@panates.com>
2025-03-21 16:31:39 +0300

    dev: Development changes

---
+++ .github/actions/node-release-npm/src/index.mjs
@@ -0,0 +1,62 @@
+import { execSync } from "node:child_process";
+import fs from "node:fs";
+import path from "node:path";
+import * as core from "@actions/core";
+import colors from "ansi-colors";
+
+async function run() {
+  core.startGroup("Publishing npm packages");
+
+  const rootDir = process.env.GITHUB_WORKSPACE;
+  const artifactsDir = path.join(rootDir, "__artifacts__");
+  if (!fs.existsSync(artifactsDir)) {
+    core.setFailed("__artifacts__ directory do not exists");
+    return;
+  }
+
+  const projects = JSON.parse(
+    fs.readFileSync(path.join(artifactsDir, "projects.json"), "utf-8"),
+  );
+  console.log(projects);
+
+  for (const pkg of projects) {
+    if (!pkg.isNpmPackage) continue;
+
+    if (await fetchVersionFromNpm(pkg.name + "@" + pkg.version)) {
+      core.info(
+        `Package ${colors.magenta(pkg.name)}@${colors.magenta(
+          pkg.version,
+        )} already exists on npm. Skipping`,
+      );
+      continue;
+    }
+
+    core.info(
+      `Publishing ${colors.magenta(pkg.name)}@${colors.magenta(pkg.version)}`,
+    );
+    try {
+      execSync("npm publish", {
+        cwd: path.join(artifactsDir, pkg.directory),
+        stdio: "inherit",
+      });
+    } catch (error) {
+      core.setFailed(error);
+      return;
+    }
+  }
+  core.endGroup();
+}
+
+export async function fetchVersionFromNpm(packageName) {
+  try {
+    return execSync('npm show "' + packageName + '" version')
+      .toString()
+      .trim();
+  } catch (error) {
+    console.error("Error fetching version from npm repository:", error);
+  }
+}
+
+run().catch((error) => {
+  core.setFailed(error);
+});
