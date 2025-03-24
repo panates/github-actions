16058a6dbe45152d5591991a4c8a67fcabc40897
Eray Hanoğlu <e.hanoglu@panates.com>
2025-03-24 17:38:58 +0300

    doc: Updated docs

--- .github/actions/stage-deploy/src/index.mjs
+++ .github/actions/stage-deploy/src/index.mjs
@@ -1,7 +1,7 @@
-import fs from "node:fs";
-import path from "node:path";
 import * as core from "@actions/core";
 import colors from "ansi-colors";
+import fs from "fs";
+import path from "path";
 
 async function run() {
   /** Read packages inputs */
